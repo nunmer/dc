c8dc51ca3419ad45c473c85d91417ccc4675b0a1
sqnzhqr <karamashj@gmail.com>
2022-10-25 20:38:15 +0600

    task 2

---
+++ examples/WebServer/src/main/java/org/example/WebServer.java
@@ -0,0 +1,69 @@
+package org.example;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+
+/**
+ * Simple web server.
+ */
+public class WebServer {
+    public static void main(String[] args) {
+        ThreadSafeQueue queue = new ThreadSafeQueue();
+
+        // Port number for http request
+        int port = args.length > 1 ? Integer.parseInt(args[1]) : 8080;
+
+        // The maximum queue length for incoming connection
+        int queueLength = args.length > 2 ? Integer.parseInt(args[2]) : 50;
+
+        int numOfThreads = 6;
+
+        for (int i = 0; i < numOfThreads; i++) {
+            Consumer cons = new Consumer(queue);
+            cons.start();
+        }
+
+        try (ServerSocket serverSocket = new ServerSocket(port, queueLength)) {
+            System.out.println("Web Server is starting up, listening at port " + port + ".");
+            System.out.println("You can access http://localhost:" + port + " now.");
+
+            while (true) {
+                // Make the server socket wait for the next client request
+                Socket socket = serverSocket.accept();
+
+                System.out.println("Got connection!");
+
+                // To read input from the client
+                BufferedReader input = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+
+                try {
+                    // Get request
+                    HttpRequest request = HttpRequest.parse(input);
+
+                    // Process request
+                    RequestHandler proc = new RequestHandler(socket, request);
+
+                    queue.add(proc);
+
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+
+
+            }
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            for (int i = 0; i < numOfThreads; i++) {
+                queue.add(null);
+            }
+            System.out.println("Server has been shutdown!");
+        }
+    }
+}
